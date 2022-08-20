98824bc16a4b0b207cf3feb59167c21616fe67d1
Tomas Toloza <tomasezequieltoloza@gmail.com>
2022-08-20 11:21:40 -0300

    Clase 1

---
+++ src/clase1.hs
@@ -0,0 +1,12 @@
+doble x = 2 * x
+suma x y = x + y
+producto x y = x * y
+cuadrado x = x ^ 2
+constante x = x
+f1 x1 x2 = sqrt((x1^2) + (x2^2))
+
+absoluto x | x >= 0    = x
+           | otherwise = -x
+
+absoluto' x = if x >= 0 then x else -x
+
